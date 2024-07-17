c705f2388f7e4095152a391a4fe27594c47ba43f
ChinaCapta1n <workingluffy@gmail.com>
2024-07-17 21:34:45 +0800

    aaaa

--- script/index.js
+++ script/index.js
@@ -1,23 +1,110 @@
-(() => {
+const oImageFileSelector = document.querySelector('#imageFileSelector');
+const oOriginImagePreview = document.querySelector('#originImagePreview');
+const oCompressImagePreview = document.querySelector('#compressedImagePreview');
+const reader = new FileReader();
 
-    const canvas = document.querySelector('canvas');
-    const downloadLink = document.querySelector('#download');
+let imageFile = null;
+let quality = 30;
+let compressedImageSource = '';
 
-    const ctx = canvas.getContext('2d');
 
-    const img = new Image();
-    img.src = './image/Shopping.jpg';
+const IMAGE_TYPES = {
+  'image/jpeg': 'image/jpeg',
+  'image/png': 'image/png',
+  'image/webp': 'image/webp'
+}
 
-    img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
+const init = () => {
+  bindEvent();
+}
 
-        const imgData = canvas.toDataURL('image/webp', 1.0);
+function bindEvent () {
+  oImageFileSelector.addEventListener('change', handleFileSelectorChange, false);
+}
 
-        downloadLink.download = 'output_image.webp';
-        downloadLink.href = imgData;
-        downloadLink.click();
+function handleFileSelectorChange(e) {
+  imageFile = e.target.files[0];
+  
+  if(!imageFile || !IMAGE_TYPES[imageFile.type]) {
+    alert('请选择正确格式的图片');
+    setImageFileEmpty(); // 即使选择不支持类型，也依然会被选取，必须手动清空
+    return;
+  }
+
+  setImagePreview(imageFile);
+}
+
+function setImageFileEmpty() {
+  oImageFileSelector.value = '';
+  imageFile = null;
+
+  setPreviewVisible(oOriginImagePreview, false);
+  setPreviewVisible(oCompressImagePreview, false);
+}
+
+function setImagePreview(imageFile) {
+  if(imageFile instanceof File) {
+    reader.onload = async () => {
+      const originImageSource = reader.result;
+       await createCompressedImage({
+        imageSource: originImageSource,
+        quality,
+        type: imageFile.type
+      })
+
+      oOriginImagePreview.src = originImageSource;
+      oCompressImagePreview.src = compressedImageSource;
+      setPreviewVisible(oOriginImagePreview, true);
+      setPreviewVisible(oCompressImagePreview, true);
     }
 
-})();
+    reader.readAsDataURL(imageFile);
+  }
+}
+
+function createCompressedImage({ imageSource, type }) {
+  const canvas = document.createElement('canvas');
+  const image = document.createElement('img');
+  const ctx = canvas.getContext('2d');
+
+  image.src = imageSource;
+
+  return new Promise((resolve) => {
+    image.onload = () => {
+      const imageWidth = image.width;
+      const imageHeight = image.height;
+  
+      canvas.width = imageWidth;
+      canvas.height = imageHeight;
+  
+      ctx.drawImage(image, 0, 0, imageWidth, imageHeight);
+  
+      doCompress(canvas, imageSource, type);
+      resolve(compressedImageSource);
+    }
+  })
+}
+
+function doCompress(canvas, imageSource, type) {
+  compressedImageSource = canvas.toDataURL(type, quality / 100);
+
+  if(compressedImageSource.length >= imageSource.length && quality > 10) {
+    quality -= 10;
+    doCompress(canvas, imageSource, type);
+  }
+}
+
+function setPreviewVisible(node, visible) {
+  switch(visible) {
+    case true:
+      node.classList.remove('hide');
+      node.classList.add('show');
+      break;
+    case false:
+      node.classList.remove('show');
+      node.classList.add('hide');
+      break;
+  }
+}
+
+init();
